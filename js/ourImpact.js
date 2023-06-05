8067df7000031b5717fe31274a42de39003f62b9
limatfc <thaisafc.lima@outlook.com>
2023-06-05 19:58:02 +0200

    adding functionality

---
+++ js/ourImpact.js
@@ -0,0 +1,59 @@
+function toggleButtonOne(boo) {
+  const openButtonOne = document.getElementById("open-one");
+  const closeButtonOne = document.getElementById("close-one");
+  if (boo) {
+    openButtonOne.style.display = "block";
+    closeButtonOne.style.display = "none";
+    return;
+  }
+  openButtonOne.style.display = "none";
+  closeButtonOne.style.display = "block";
+}
+
+function toggleButtonTwo(boo) {
+  const openButtonOne = document.getElementById("open-two");
+  const closeButtonOne = document.getElementById("close-two");
+  if (boo) {
+    openButtonOne.style.display = "block";
+    closeButtonOne.style.display = "none";
+    return;
+  }
+  openButtonOne.style.display = "none";
+  closeButtonOne.style.display = "block";
+}
+
+function toggleButtonThree(boo) {
+  const openButtonOne = document.getElementById("open-three");
+  const closeButtonOne = document.getElementById("close-three");
+  if (boo) {
+    openButtonOne.style.display = "block";
+    closeButtonOne.style.display = "none";
+    return;
+  }
+  openButtonOne.style.display = "none";
+  closeButtonOne.style.display = "block";
+}
+
+function toggleButtonFour(boo) {
+  const openButtonOne = document.getElementById("open-four");
+  const closeButtonOne = document.getElementById("close-four");
+  if (boo) {
+    openButtonOne.style.display = "block";
+    closeButtonOne.style.display = "none";
+    return;
+  }
+  openButtonOne.style.display = "none";
+  closeButtonOne.style.display = "block";
+}
+
+function toggleButtonFive(boo) {
+  const openButtonOne = document.getElementById("open-five");
+  const closeButtonOne = document.getElementById("close-five");
+  if (boo) {
+    openButtonOne.style.display = "block";
+    closeButtonOne.style.display = "none";
+    return;
+  }
+  openButtonOne.style.display = "none";
+  closeButtonOne.style.display = "block";
+}
